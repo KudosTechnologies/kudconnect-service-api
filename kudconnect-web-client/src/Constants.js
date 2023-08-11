d9f332d880f3370a2b421ac7c9f2d0668ae19d27
andrei.rad <rad29ndrei@gmail.com>
2023-08-11 23:49:26 +0300

    KDC-52: custom-claim-mapper

--- kudconnect-web-client/src/Constants.js
+++ kudconnect-web-client/src/Constants.js
@@ -8,15 +8,12 @@ const prod = {
 
 const dev = {
     url: {
-        KEYCLOAK_BASE_URL: "http://kudconnect.local:9080",
-        API_BASE_URL: 'http://kudconnect.local:8080',
+        // KEYCLOAK_BASE_URL: "http://kudconnect.local:9080",
+        // API_BASE_URL: 'http://kudconnect.local:8080',
+        KEYCLOAK_BASE_URL: "http://localhost:9080",
+        API_BASE_URL: 'http://localhost:8080',
         AVATARS_DICEBEAR_URL: 'https://api.dicebear.com/6.x'
     }
-    // url: {
-    //     KEYCLOAK_BASE_URL: "http://localhost:9080",
-    //     API_BASE_URL: 'http://localhost:8080',
-    //     AVATARS_DICEBEAR_URL: 'https://api.dicebear.com/6.x'
-    // }
 }
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod
